150db1f46efdde4fe879968a639a4928b68d17f0
sword0611 <tristandekok@gmail.com>
2019-10-20 23:49:00 +0200

    website

---
+++ javascript/javascript.js
@@ -0,0 +1,11 @@
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "305px";
+    document.getElementById("mySidepanel").style.height = "1080px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+
